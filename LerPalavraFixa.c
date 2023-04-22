3bae3afaebb1f2ec3c5c91666bfba9e04af76da0
MiguelAndrade866 <a31366@tomazpelayo.com>
2023-04-22 17:32:54 +0100

    exercicio16

--- LerPalavraFixa.c
+++ LerPalavraFixa.c
@@ -1,3 +1,13 @@
-// Resolva neste ficheiro o seu exercício
-
+#include <stdio.h>
+int main ()
+{
+    char caractere1,caractere2,caractere3;
+    caractere1 = getc(stdin);
+    caractere2 = getc(stdin);
+    caractere3 = getc(stdin);
+    putc(caractere1, stdout);
+    putc(caractere2, stdout);
+    putc(caractere3, stdout);
+    return(0);
+}
 
